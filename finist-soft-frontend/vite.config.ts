1e5fb2aac30f78fbfb6b24ba74bbe40592670937
Lebedeff <106642323+ialebedeff@users.noreply.github.com>
2024-02-05 10:29:16 +0300

    vite.config changed, version up

--- finist-soft-frontend/vite.config.ts
+++ finist-soft-frontend/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: "esnext",
+    outDir: "dist",
+    assetsDir: "",
+    assetsInlineLimit: 0,
+    cssCodeSplit: false,
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    port: 4173,
+    host: "0.0.0.0",
+  },
+});
